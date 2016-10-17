42a092ba5cd30d69505a1291d481bc688b905c01
CJ Powers <cjpwrs@gmail.com>
2016-10-16 21:53:59 -0600

    random stuff

--- src/components/HomeLandingPage/ImageHolder.js
+++ src/components/HomeLandingPage/ImageHolder.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {Link} from 'react-router';
+import {connect} from 'react-redux';
+import {bindActionCreators} from 'redux';
 import Masonry from 'react-masonry-component';
 
 require('./ImageHolder.css');
@@ -8,8 +10,34 @@ var masonryOptions = {
     transitionDuration: 0
 };
 
-const ImageHolder = ({dataset}) => {
-    var images = dataset.map(image=>{
+const loadingGif = (<div className='loading'>
+    <img src='https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif' />
+  </div>);
+
+class ImageHolder extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      classes: 'image-holder'
+    }
+    this.show = this.show.bind(this);
+    this.handleImagesLoaded = this.handleImagesLoaded.bind(this);
+  }
+  show(){
+    this.setState({ classes: 'image-holder', loading: false })
+  }
+  hide(){
+    this.setState({ classes: 'image-holder hide', loading: true })
+  }
+  componentDidMount() {
+      this.hide();
+  }
+  handleImagesLoaded(imagesLoadedInstance) {
+      this.show();
+  }
+
+  render() {
+    var images = this.props.dataset.map(image=>{
       return (
         <div key={image.id} className='image-element-class img-div'>
           <img src={image.url} />
@@ -17,15 +45,30 @@ const ImageHolder = ({dataset}) => {
       )
     })
     return (
-      <Masonry
-              className={'img-container'}
-              options={masonryOptions}
-              disableImagesLoaded={false}
-              updateOnEachImageLoad={false} >
-        {images}
-      </Masonry>
+      <div className={this.state.classes}>
+        {this.state.loading? loadingGif: null}
+        <Masonry
+                onImagesLoaded={this.handleImagesLoaded}
+                className={'img-container'}
+                options={masonryOptions}
+                disableImagesLoaded={false}
+                updateOnEachImageLoad={true} >
+          {images}
+        </Masonry>
+      </div>
     )
+  }
 }
 
 
-export default ImageHolder;
+function mapStateToProps(state, ownProps) {
+    return {
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ImageHolder);
